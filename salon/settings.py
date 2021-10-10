3426f745f0a5f98ca495ae4c8e80d4583ba43e8b
Bonface Mwicwiri <67801303+smartcoder-developer@users.noreply.github.com>
2021-10-10 19:08:22 +0300

    Update settings.py

--- salon/settings.py
+++ salon/settings.py
@@ -114,8 +114,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_SUBJECT_PREFIX = "Salon"
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = "mwicwiribonface@gmail.com"
-EMAIL_HOST_PASSWORD = "153@921@@@1IslaKJ"
+EMAIL_HOST_USER = 
+EMAIL_HOST_PASSWORD = 
 EMAIL_PORT = 587
 
 # Internationalization
